46392d655c4e34f2e650d9728c2946d3f0bbd38b
nedu'm <davidsonchinedum@gmail.com>
2022-04-04 21:12:49 +0100

    init

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import { StyledHeader } from "./styles/Header.styled"
+
+export default function Header() {
+  return (
+    <StyledHeader>
+      <h1>Xend</h1>
+    </StyledHeader>
+  )
+}
---
+++ src/components/styles/Header.styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const StyledHeader = styled.header`
+background-color: white;
+padding: 40px 0;
+`;
