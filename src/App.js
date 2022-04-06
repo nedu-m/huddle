402d427da8947c170ea43121fde6ccf0c69ad2f1
nedu'm <davidsonchinedum@gmail.com>
2022-04-06 13:25:34 +0100

    built nav and logo component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import Header from './components/Header'
-import { Container } from './components/styles/Container.styled'
+import GlobalStyles from './components/styles/Global';
+import { ThemeProvider } from 'styled-components';
+import Header from './components/Header';
+import { Container } from './components/styles/Container.styled';
+import Theme from './components/styles/Theme.styled';
+
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme = {Theme}>
+      <>
+      <GlobalStyles />
       <Header />
       <Container>
         <h1>Hello world</h1>
       </Container>
-    </>
+      </>
+    </ThemeProvider>
   );
+  
 }
 
 export default App;
