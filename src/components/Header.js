6b272794ce49d154bd74d21d9f4dc55c277546b7
nedu'm <davidsonchinedum@gmail.com>
2022-04-06 13:44:31 +0100

    built navbar component and added content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { StyledHeader, Nav, Logo } from "./styles/Header.styled";
 import { Container } from "./styles/Container.styled";
-import { Button } from  "./styles/Button.styled"
+import { Button } from "./styles/Button.styled"
+import { Flex } from "./styles/Flex.styled"
 
 export default function Header() {
   return (
@@ -12,6 +13,24 @@ export default function Header() {
             Try it Free
           </Button>
         </Nav>
+
+        <Flex>
+          <div>
+            <h1>
+              Build for The Community, Your Fans Will Love It
+            </h1>
+
+            <p>
+              Huddle re-imagines the way we build communities. You have a voice,
+              but so does your audience. Create connections with your users as
+              you engage in genuine discussion.
+            </p>
+
+            <Button bg='#ff0099' color="#fff">
+            Get Started for Free
+            </Button>
+          </div>
+        </Flex>
       </Container>
     </StyledHeader>
   )
